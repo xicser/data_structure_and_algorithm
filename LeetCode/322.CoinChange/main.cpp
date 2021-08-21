883d51125357b4326fc855bdd5d042833780e497
WangXi@School <xicser@126.com>
2021-08-21 18:48:34 +0800

    add 322.

---
+++ LeetCode/322.CoinChange/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+
+        int bagSize = amount;
+        int thingCnt = coins.size();
+
+
+        vector<int> dp(bagSize + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 0; i < thingCnt; i++) {
+            for (int j = coins[i]; j <= bagSize; j++) {
+                if (dp[j - coins[i]] != INT_MAX) {
+                    dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+
+        return dp[bagSize] == INT_MAX ? -1 : dp[bagSize];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> coins1 = {1, 2, 5};
+    vector<int> coins2 = {2};
+
+    cout << sol.coinChange(coins1, 11) << endl;
+    cout << sol.coinChange(coins2, 3) << endl;
+
+    return 0;
+}
