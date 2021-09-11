4674b785c19c835c4a2ac6d3883fdaf5e319ef4d
WangXi@College <xicser@126.com>
2021-09-11 22:14:47 +0800

    add 102, 107, 199, 637.

--- LeetCode/dp/Subsequence/1143.LongestCommonSubsequence/main.cpp
+++ LeetCode/dp/Subsequence/1143.LongestCommonSubsequence/main.cpp
@@ -11,48 +11,31 @@ public:
 
         int len1 = text1.size();
         int len2 = text2.size();
-        int res = 0;
+        int res;
 
         // dp[i][j] 表示A数组中i位置(含)之前
 		// 和
 		// B数组中j位置(含)之前
 		// 最长重复子序列的长度
-        vector< vector<int> > dp(len1, vector<int>(len2, 0) );
+        vector< vector<int> > dp(len1 + 1, vector<int>(len2 + 1, 1) );
 
         //dp数组初始化
-        for (int i = 0; i < len1; i++) {
-            if (text1[i] == text2[0]) {
-                dp[i][0] = 1;
-                res = 1;
-                for (int k = i + 1; k < len1; k++) {
-                    dp[k][0] = 1;
-                }
-            }
-        }
-        for (int j = 0; j < len2; j++) {
-            if (text1[0] == text2[j]) {
-                dp[0][j] = 1;
-                res = 1;
-                for (int k = j + 1; k < len2; k++) {
-                    dp[0][k] = 1;
-                }
-            }
-        }
+        res = 1;
 
-        for (int i = 1; i < len1; i++) {
-            for (int j = 1; j < len2; j++) {
+        for (int i = 1; i < len1 + 1; i++) {
+            for (int j = 1; j < len2 + 1; j++) {
 
-                if (text1[i] == text2[j]) {
+                if (text1[i - 1] == text2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
-                    res = max(res, dp[i][j]);
                 } else {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                    res = max(res, dp[i][j]);
                 }
+
+                res = max(res, dp[i][j]);
             }
         }
 
-        return res;
+        return res - 1;
     }
 };
 
