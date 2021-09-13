7c08c570c48e9c4bd74e9964da2d405d39429ce4
WangXi@College <xicser@126.com>
2021-09-13 16:57:33 +0800

    add 112, 113.

---
+++ LeetCode/tree/112.PathSum/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        this->targetSum = targetSum;
+        hasPath = false;
+        int sum = 0;
+        process(root, sum);
+
+        return hasPath;
+    }
+
+    void process(TreeNode* root, int sum) {
+
+        if (root == nullptr) {
+            return;
+        }
+
+        if (root->left == nullptr && root->right == nullptr) {
+            sum += root->val;
+            if (sum == this->targetSum) {
+                hasPath = true;
+            }
+            return;
+        }
+
+        sum += root->val;
+        process(root->left, sum);
+        process(root->right, sum);
+    }
+
+private:
+    vector<int> pathSums;
+    int targetSum;
+    bool hasPath;
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ LeetCode/tree/113.PathSum-II/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector< vector<int> > pathSum(TreeNode* root, int targetSum) {
+
+        this->targetSum = targetSum;
+
+        vector<int> path;
+        int sum;
+        process(root, path, sum);
+        return paths;
+    }
+
+private:
+    void process(TreeNode* root, vector<int> path, int sum) {
+
+        if (root == nullptr) {
+            return;
+        }
+
+        if (root->left == nullptr && root->right == nullptr) {
+
+            path.push_back(root->val);
+            sum += root->val;
+
+            if (sum == this->targetSum) {
+                paths.push_back(path);
+            }
+
+            return;
+        }
+
+        path.push_back(root->val);
+        sum += root->val;
+        process(root->left, path, sum);
+        process(root->right, path, sum);
+    }
+
+    vector< vector<int> > paths;
+    int targetSum;
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
