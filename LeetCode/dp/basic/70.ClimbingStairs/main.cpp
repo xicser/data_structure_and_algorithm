c832cd3cd8b74a0ea3e81eff9d47063059614135
WangXi@College <xicser@126.com>
2021-10-08 20:47:45 +0800

    add 70.ClimbingStairs.

--- LeetCode/dp/basic/70.ClimbingStairs/main.cpp
+++ LeetCode/dp/basic/70.ClimbingStairs/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -14,18 +15,35 @@ public:
 //        return climbStairs(n - 1) + climbStairs(n - 2);
 //    }
 
+//    int climbStairs(int n) {
+//
+//        if (n <= 2) {
+//            return n;
+//        }
+//
+//        int dp[n + 1];   //第i层楼梯, 一共有dp[i]种上法
+//
+//        dp[1] = 1;
+//        dp[2] = 2;
+//        for (int i = 3; i <= n; i++) {
+//            dp[i] = dp[i - 1] + dp[i - 2];
+//        }
+//
+//        return dp[n];
+//    }
+
     int climbStairs(int n) {
 
-        if (n <= 2) {
-            return n;
-        }
+        vector<int> things = {1, 2};
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
 
-        int dp[n + 1];   //第i层楼梯, 一共有dp[i]种上法
-
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for (int j = 1; j <= n; j++) {
+            for (int i = 0; i < things.size(); i++) {
+                if (j >= things[i]) {
+                    dp[j] += dp[j - things[i]];
+                }
+            }
         }
 
         return dp[n];
