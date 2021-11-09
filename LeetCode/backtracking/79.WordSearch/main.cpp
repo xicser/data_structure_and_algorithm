25aa8d33dd9b13a10b93f59ed07662b98c05a3af
WangXi@Desktop <xicser@126.com>
2021-11-09 14:46:45 +0800

    add 79.

--- LeetCode/backtracking/79.WordSearch/main.cpp
+++ LeetCode/backtracking/79.WordSearch/main.cpp
@@ -8,124 +8,57 @@ class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
 
-        index = 0;
-        this->word = word;
-        hasFound = false;
         rowCnt = board.size();
         colCnt = board[0].size();
 
         //表示某个位置是否被访问过
         vector< vector<bool> > visit(rowCnt, vector<bool>(colCnt, false) );
 
-        //搜索各个位置
+        //从各个位置开始搜索
         for (int i = 0; i < rowCnt; i++) {
-            if (hasFound == true) {
-                break;
-            }
-
             for (int j = 0; j < colCnt; j++) {
-
-                if (hasFound == true) {
-                    break;
+                if (dfs(board, visit, i, j, word, 0) == true) {
+                    return true;
                 }
-
-                if (word[index] != board[i][j]) {
-                    continue;
-                }
-
-                index++;
-                path.push_back(board[i][j]);
-                visit[i][j] = true;
-                backTracking(i, j, board, visit);
-                path.pop_back();
-                visit[i][j] = false;
-                index--;
-
-                if (hasFound == true) {
-                    break;
-                }
-            }
-
-            if (hasFound == true) {
-                break;
             }
         }
 
-        return hasFound;
+        return false;
     }
 
 private:
-    string word;
     int rowCnt;
     int colCnt;
-    bool hasFound;
-    string path;
-    int index;
 
-    void backTracking(int nowRow, int nowCol,
-                      vector<vector<char>>& board,
-                      vector< vector<bool> >& visit) {
-
-        if (hasFound == true) {
-            return;
-        }
-        if (path.size() == word.size()) {
-            if (path == word) {
-                hasFound = true;
-            }
-            return;
+    bool dfs(vector<vector<char>>& board, vector< vector<bool> >& visit,
+             int x, int y,
+             string &word, int pos)
+    {
+        //范围超出棋盘边界了, 或者已经被访问过了
+        if (x < 0 || y < 0 || x >= rowCnt || y >= colCnt || visit[x][y] == true) {
+            return false;
         }
 
-        //根据当前位置, 获取下一跳能够到达的所有位置
-        vector< vector<int> > nextPos = getNext(nowRow, nowCol, visit);
-        for (unsigned int i = 0; i < nextPos.size(); i++) {
-
-            if (hasFound == true) {
-                return;
-            }
-
-            vector<int> pos = nextPos[i];
-            int x = pos[0];
-            int y = pos[1];
-
-            if (word[index] != board[x][y]) {
-                continue;
-            }
-
-            index++;
-            visit[x][y] = true;
-            path.push_back(board[ x ][ y ]);
-
-            backTracking(x, y, board, visit);
-
-            if (hasFound == true) {
-                return;
-            }
-
-            index--;
-            visit[x][y] = false;
-            path.pop_back();
+        if (board[x][y] != word[pos]) {
+            return false;
         }
-    }
 
-    //根据当前位置, 获取下一跳能够到达的位置
-    vector< vector<int> > getNext(int row, int col, vector< vector<bool> >& visit) {
+        if (pos == word.size() - 1) {
+            return true;
+        }
 
-        vector< vector<int> > nextPos;
+        //记录已经被访问
+        visit[x][y] = true;
 
-        if (row - 1 >= 0 && visit[row - 1][col] == false) {
-            nextPos.push_back({row - 1, col});
-        }
-        if (row + 1 <= rowCnt - 1 && visit[row + 1][col] == false) {
-            nextPos.push_back({row + 1, col});
-        }
-        if (col - 1 >= 0 && visit[row][col - 1] == false) {
-            nextPos.push_back({row, col - 1});
-        }
-        if (col + 1 <= colCnt - 1 && visit[row][col + 1] == false) {
-            nextPos.push_back({row, col + 1});
-        }
-        return nextPos;
+        bool result = dfs(board, visit, x - 1, y, word, pos + 1) ||   //上
+               dfs(board, visit, x + 1, y, word, pos + 1) ||   //下
+               dfs(board, visit, x, y - 1, word, pos + 1) ||   //左
+               dfs(board, visit, x, y + 1, word, pos + 1);     //右
+
+        //回溯
+        visit[x][y] = false;
+
+        return result;
     }
 };
 
