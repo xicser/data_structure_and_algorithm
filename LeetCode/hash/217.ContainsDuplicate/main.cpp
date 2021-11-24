be6111e785953facf9bb4fecb82eb8a25f31541d
WangXi@Desktop <xicser@126.com>
2021-11-24 11:54:32 +0800

    add 827, 217, 1226.

---
+++ LeetCode/hash/217.ContainsDuplicate/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> numSet;
+
+        for (unsigned int i = 0; i < nums.size(); i++) {
+            if (numSet.find(nums[i]) == numSet.end()) {
+                numSet.insert(nums[i]);
+            }
+            else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ LeetCode/thread/1226.TheDiningPhilosophers/1226.TheDiningPhilosophers.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <functional>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+class DiningPhilosophers {
+private:
+    mutex forks[5];
+    mutex lock;
+public:
+    DiningPhilosophers() {
+
+    }
+
+    void wantsToEat(int philosopher,
+        function<void()> pickLeftFork,
+        function<void()> pickRightFork,
+        function<void()> eat,
+        function<void()> putLeftFork,
+        function<void()> putRightFork) {
+
+        int left, right;
+        if (philosopher == 0) {
+            left = 0;
+            right = 4;
+        }
+        else {
+            left = philosopher;
+            right = philosopher - 1;
+        }
+
+        //同时锁住两个
+        lock.lock();
+        forks[left].lock();
+        forks[right].lock();
+        lock.unlock();
+
+        pickLeftFork();
+        pickRightFork();
+        eat();
+        putLeftFork();
+        putRightFork();
+
+        forks[left].unlock();
+        forks[right].unlock();
+    }
+};
+
+int main()
+{
+    DiningPhilosophers diningPhilosophers;
+
+   // thread tph1(&DiningPhilosophers::wantsToEat, ref(diningPhilosophers));
+
+
+    std::cout << "Hello World!\n";
+}
+
