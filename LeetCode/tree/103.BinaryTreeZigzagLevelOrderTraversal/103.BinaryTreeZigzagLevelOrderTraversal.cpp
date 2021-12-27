d97c3223080bd58a5d0cad712193296e9c2fc747
WangXi@Laptop <xicser@126.com>
2021-12-27 11:40:17 +0800

    add 103, 95.

---
+++ LeetCode/tree/103.BinaryTreeZigzagLevelOrderTraversal/103.BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        queue<TreeNode*> que;
+        if (root != NULL) que.push(root);
+        vector < vector<int> > result;
+        bool dir = true;  //true表示从左往右
+        while (!que.empty()) {
+            int size = que.size();
+            vector<int> vec;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                vec.push_back(node->val);
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+            if (dir == false) {
+                reverse(vec.begin(), vec.end());
+                dir = true;
+            }
+            else {
+                dir = false;
+            }
+            result.push_back(vec);
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ LeetCode/tree/95.UniqueBinarySearchTreesII/95.UniqueBinarySearchTreesII.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    //函数含义：获取区间[start, end]之间，能构成二叉搜索树的集合
+    vector<TreeNode*> buildTree(int start, int end) {
+
+        if (start > end) {
+            return { nullptr };
+        }
+        if (start == end) {
+            TreeNode* rootNode = new TreeNode(start);
+            rootNode->left = rootNode->right = nullptr;
+            return { rootNode };
+        }
+
+        vector<TreeNode*> resultTrees;
+        for (int pos = start; pos <= end; pos++) {
+            vector<TreeNode*> leftTrees = buildTree(start, pos - 1);   //获取左侧子区间的树
+            vector<TreeNode*> rightTrees = buildTree(pos + 1, end);    //获取右侧子区间的树
+
+            //两两相乘获取所有的结果
+            for (int i = 0; i < leftTrees.size(); i++) {
+                for (int j = 0; j < rightTrees.size(); j++) {
+                    TreeNode* rootNode = new TreeNode(pos);
+                    rootNode->left = leftTrees[i];
+                    rootNode->right = rightTrees[j];
+                    resultTrees.push_back(rootNode);
+                }
+            }
+        }
+
+        return resultTrees;
+    }
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        return buildTree(1, n);
+    }
+
+    void printTree(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        cout << root->val << " ";
+        printTree(root->left);
+        printTree(root->right);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<TreeNode*> result = sol.generateTrees(3);
+    for (int i = 0; i < result.size(); i++) {
+        sol.printTree(result[i]);
+        cout << endl;
+    }
+}
