f35a15a6c5d248c0e69615d62a1266c5cf815d7d
WangXi@Laptop <xicser@126.com>
2021-12-27 16:29:34 +0800

    add 129.

---
+++ LeetCode/tree/129.SumRootToLeafNumbers/129.SumRootToLeafNumbers.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+
+        backtraking(root);
+        int result = 0;
+
+        for (int pathSum : resultPath) {
+            result += pathSum;
+        }
+
+        return result;
+    }
+
+private:
+    vector<int> resultPath;
+    string path;
+    void backtraking(TreeNode* root) {
+
+        if (root == nullptr) {
+            return;
+        }
+
+        if (root->left == nullptr && root->right == nullptr) {
+            
+            path.push_back(root->val + 48);
+            
+            int pathSum = atoi(path.c_str());
+            //cout << pathSum << endl;
+            resultPath.push_back(pathSum);
+            
+            path.pop_back();
+            return;
+        }
+
+        char val = root->val + 48;
+
+        //回溯的流程
+        path.push_back(val);
+        backtraking(root->left);
+        path.pop_back();
+
+        path.push_back(val);
+        backtraking(root->right);
+        path.pop_back();
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
