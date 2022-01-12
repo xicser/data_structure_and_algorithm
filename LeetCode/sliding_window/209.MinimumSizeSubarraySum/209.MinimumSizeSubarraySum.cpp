14234e22b65ff7a164bd0c76b69f8e1d3669b1d0
WangXi@Laptop <xicser@126.com>
2022-01-12 22:45:13 +0800

    add 209, 76.

---
+++ LeetCode/sliding_window/209.MinimumSizeSubarraySum/209.MinimumSizeSubarraySum.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0, right = 0;
+        int windowSum = 0;
+        int winSizeMin = INT_MAX;
+
+        while (right < nums.size()) {
+            windowSum += nums[right];
+            right++;
+
+            while (windowSum >= target) {
+
+                int winSize = right - left;
+                if (winSizeMin > winSize) {
+                    winSizeMin = winSize;
+                }
+
+                windowSum -= nums[left];
+                left++;
+            }
+        }
+
+        if (winSizeMin == INT_MAX) {
+            return 0;
+        }
+        return winSizeMin;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ LeetCode/sliding_window/76.MinimumWindowSubstring/76.MinimumWindowSubstring.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+//s = "ADOBEC ODEBANC", t = "ABC"
+class Solution {
+private:
+    //检查当前的window能否满足need的要求
+    bool checkWindow(unordered_map<char, int>& window,
+                     unordered_map<char, int>& need) {
+        for (auto it : need) {
+            char c = it.first;
+            int cnt = it.second;
+
+            if (window.count(c) == 0 || //当前窗口不存在need中的某个字符
+                window[c] < cnt) {      //或者数量不够
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    string minWindow(string s, string t) {
+
+        int left = 0, right = 0;
+        string result;   //记录结果
+        int resultSize = INT_MAX; //结果长度
+
+        unordered_map<char, int> window;  //当前窗口中每个字符出现的次数
+        unordered_map<char, int> need;    //根据t计算的需要的字符个数
+        for (char c : t) {
+            need[c]++;
+        }
+
+        while (right < s.size()) {
+            window[ s[right] ]++;
+            right++;
+
+            //当前窗口满足条件
+            while (checkWindow(window, need) == true) {
+                int winSize = right - left;
+                if (winSize < resultSize) {
+                    resultSize = winSize;
+                    result = s.substr(left, winSize);
+                }
+                window[ s[left] ]--;
+                left++;
+            }
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.minWindow("a", "aa") << endl;
+}
