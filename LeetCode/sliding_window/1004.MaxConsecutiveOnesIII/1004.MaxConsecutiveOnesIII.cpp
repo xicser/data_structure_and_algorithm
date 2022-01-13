5f3cd542aedec6f0347dcd4467980ed01dfc3812
WangXi@Laptop <xicser@126.com>
2022-01-13 21:07:14 +0800

    add 1004, 424.

---
+++ LeetCode/sliding_window/1004.MaxConsecutiveOnesIII/1004.MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 输入：A = [1,1,1,0,  0,0,1,1,1,1], K = 0
+/*
+重点：题意转换。把「最多可以把 K 个 0 变成 1，求仅包含 1 的最长子数组的长度」转换为 「找出一个最长的子数组，该子数组内最多允许有 K 个 0 」。
+*/
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0, right = 0;
+        int winZeroCnt = 0;  //记录当前窗口中0的个数
+        int result = INT_MIN;
+
+        while (right < nums.size()) {
+            if (nums[right] == 0) {
+                winZeroCnt++;
+            }
+            right++;
+
+            while (winZeroCnt > k) {
+                if (nums[left] == 0) {
+                    winZeroCnt--;
+                }
+                left++;
+            }
+            int winSize = right - left;
+            result = max(result, winSize);
+        }
+
+        if (result == INT_MIN) {
+            return nums.size();
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = { 1,1,1,1,1,0,1,1,1,1 };
+    
+    std::cout << endl << sol.longestOnes(nums, 0) << endl;
+}
---
+++ LeetCode/sliding_window/424.LongestRepeatingCharacterReplacement/424.LongestRepeatingCharacterReplacement.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+private:
+    //判断window中, 出现次数较少的字符的个数是否大于k
+    bool check(unordered_map<char, int>& winMap, int k) {
+
+        //记录出现次数最多的字符
+        char c;
+        int cnt = INT_MIN;
+        int cntAll = 0;
+        for (auto it : winMap) {
+            char cTmp = it.first;
+            int cntTmp = it.second;
+            cntAll += cntTmp;
+
+            if (cnt < cntTmp) {
+                cnt = cntTmp;
+                c = cTmp;
+            }
+        }
+        int resCnt = cntAll - cnt;
+
+        return resCnt > k;
+    }
+
+public:
+    //"ABABCDEFG", k = 2
+    int characterReplacement(string s, int k) {
+
+        int result = 0;
+        int left = 0, right = 0;
+        int winCnt = 0;
+        unordered_map<char, int> winMap;
+
+        while (right < s.size()) {
+            winMap[ s[right] ]++;
+            right++;
+
+            //如果窗口出现次数较少的字符的个数已经超过了k
+            while (check(winMap, k) == true) {
+                winMap[ s[left] ]--;
+                left++;
+            }
+
+            //记录最大的
+            int winSize = right - left;
+            result = max(winSize, result);
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::cout << sol.characterReplacement("AABABBA", 1) << endl;
+}
