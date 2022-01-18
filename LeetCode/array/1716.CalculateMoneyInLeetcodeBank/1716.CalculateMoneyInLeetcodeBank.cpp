d814df471fb7c8fb0d5ab9f73484f27f8769c527
WangXi@Laptop <xicser@126.com>
2022-01-18 16:05:04 +0800

    add 1716, 539.

---
+++ LeetCode/array/1716.CalculateMoneyInLeetcodeBank/1716.CalculateMoneyInLeetcodeBank.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int totalMoney(int n) {
+
+        int cnt = n / 7;
+        int rest = n % 7;
+
+        int start, end;
+        int sum = 0;
+        int i = 0;
+        for (i = 0; i < cnt; i++) {
+
+            start = 1 + i;
+            end = start + 6;
+
+            sum += (start + end) * 7 / 2;
+        }
+        
+        start = i + 1;
+
+        for (int i = 0; i < (n - 7 * cnt); i++) {
+            sum += start;
+            start++;
+        }
+
+        return sum;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    cout << sol.totalMoney(4) << endl;
+}
---
+++ LeetCode/string/539.MinimumTimeDifference/539.MinimumTimeDifference.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+private:
+    // 23:59
+    int convertToMinutes(string time) {
+
+        string subHour = time.substr(0, 2);
+        string subMin = time.substr(3, 2);
+
+        if (subHour == "00" && subMin == "00") {
+            subHour = "23";
+            subMin = "60";
+        }
+
+        int h = atoi(subHour.c_str());
+        int m = atoi(subMin.c_str());
+
+        return 60 * h + m;
+    }
+
+public:
+    int findMinDifference(vector<string>& timePoints) {
+
+        if (timePoints.size() > 1440) {
+            return 0;
+        }
+
+        vector<int> timesMin;
+        for (int i = 0; i < timePoints.size(); i++) {
+            timesMin.push_back(convertToMinutes(timePoints[i]));
+        }
+        sort(timesMin.begin(), timesMin.end());
+
+        int minDelta = INT_MAX;
+        for (int i = 1; i < timesMin.size(); i++) {
+            int delta = abs(timesMin[i] - timesMin[i - 1]);
+            minDelta = min(minDelta, delta);
+        }
+
+        //第一个和最后一个反向差有可能最小
+        int perhaps = 1440 - timesMin[timesMin.size() - 1] + timesMin[0];
+
+        return min(perhaps, minDelta);
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<string> timePoints = { "05:31", "22:08", "00:35" };
+    cout << sol.findMinDifference(timePoints) << endl;
+}
