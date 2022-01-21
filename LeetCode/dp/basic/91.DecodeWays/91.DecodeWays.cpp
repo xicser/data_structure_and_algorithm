54a8b09e81a971da46def793a7ec5374221390b6
WangXi@Laptop <xicser@126.com>
2022-01-21 18:15:11 +0800

    add 91, 147.

---
+++ LeetCode/dp/basic/91.DecodeWays/91.DecodeWays.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// "22 6"
+class Solution {
+public:
+    int numDecodings(string s) {
+        //只要含有前导0, 结果就是0
+        if (s[0] == '0') {
+            return 0;
+        }
+        int len = s.size();
+        if (len == 1) {
+            //只要含有前导0, 结果就是0
+            if (s[0] == '0') {
+                return 0;
+            }
+            return 1;
+        }
+
+        //dp[i]表示[0] -> [i]之间, 的解码方法数
+        vector<int> dp(len, 0);
+        dp[0] = 1;
+
+        int a = s[1] - '0';
+        int b = (s[0] - '0') * 10 + (s[1] - '0');
+        //1位置的数当作单独的一个解码方法
+        if (1 <= a && a <= 9) {
+            dp[1] = 1;
+        }
+        //1位置和0位置的数共同当作一个解码方法
+        if (10 <= b && b <= 26) {
+            dp[1] += 1;
+        }
+
+        for (int i = 2; i < len; i++) {
+            int a = s[i] - '0';
+            int b = (s[i - 1] - '0') * 10 + (s[i] - '0');
+
+            //i位置的数当作单独的一个解码方法
+            if (1 <= a && a <= 9) {
+                dp[i] = dp[i - 1];
+            }
+            //i位置和i - 1位置的数共同当作一个解码方法
+            if (10 <= b && b <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[len - 1];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::cout << sol.numDecodings("123208") << endl;
+}
---
+++ LeetCode/list/147.InsertionSortList/147.InsertionSortList.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+
+        //空链表
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        //只有一个节点, 已经有序了
+        if (head->next == nullptr) {
+            return head;
+        }
+
+        ListNode dummy(-9999, head);  //伪头节点(默认值最小)
+
+        ListNode* pTail = head;
+        while (pTail != nullptr && pTail->next != nullptr) {
+
+            ListNode* pNextSave = pTail->next;
+            int val = pNextSave->val;
+            ListNode* pNextNextSave = pNextSave->next;
+
+            ListNode* pNow = &dummy;
+            ListNode* pNext = dummy.next;
+
+            //找插入位置
+            while (pNext != pNextSave) {
+
+                //找到了
+                if (pNow->val <= val && val <= pNext->val) {
+                    
+                    //插入
+                    pNow->next = pNextSave;
+                    pNextSave->next = pNext;
+
+                    //tail更新
+                    pTail->next = pNextNextSave;
+                    break;
+                }
+                pNow = pNow->next;
+                pNext = pNext->next;
+            }
+
+            //当前这个数已经是最大的, 即已经有序, 则直接调整tail
+            if (pNext == pNextSave) {
+                pTail = pTail->next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    ListNode* createList(vector<int>& nums) {
+
+        ListNode* head = nullptr;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            ListNode* newNode = new ListNode(nums[i]);
+            newNode->next = head;
+            head = newNode;
+        }
+
+        return head;
+    }
+
+    void printList(ListNode* head) {
+        ListNode* pCur = head;
+        while (pCur != nullptr) {
+            printf("%d ", pCur->val);
+            pCur = pCur->next;
+        }
+        printf("\n");
+    }
+};
+
+int main()
+{
+    vector<int> nums = { 2, 3, 4, 5, 6, 7 };
+    Solution sol;
+    ListNode* list = sol.createList(nums);
+    sol.printList(list);
+
+    ListNode* newList = sol.insertionSortList(list);
+    sol.printList(newList);
+}
