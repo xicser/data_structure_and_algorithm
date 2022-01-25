694600685106e39e55da41d5ea89d9dd8baacb01
WangXi@Laptop <xicser@126.com>
2022-01-25 21:38:06 +0800

    add 1688.

---
+++ LeetCode/basic/1688.CountOfMatchesInTournament/1688.CountOfMatchesInTournament.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int times = 0;
+
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+                times += n;
+            }
+            else {
+                times += n / 2;
+                n = n / 2 + 1;
+            }
+        }
+
+        return times;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    std::cout << sol.numberOfMatches(7) << std::endl;
+}
