691abb4175842adfe25bfb8c1535d8c04f997453
WangXi@Laptop <xicser@126.com>
2022-01-27 16:39:41 +0800

    add 146, 155.

---
+++ LeetCode/list/146.LRUCache/146.LRUCache.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <list>
+#include <unordered_map>
+
+using namespace std;
+
+class LRUCache {
+private:
+    int capacity;
+    unordered_map<int, list<pair<int, int>>::iterator> map; //key, value是list中元素的迭代器
+    list<pair<int, int>> list;  //pair<key, value>
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+
+    int get(int key) {
+        if (map.count(key) != 0) {
+            
+            //map[key]是对应值再list中的迭代器位置
+            auto pos = map[key];
+            int value = (*pos).second;
+
+            //更新
+            list.erase(pos);
+            list.push_front(pair<int, int>(key, value));
+            map[key] = list.begin();
+
+            return value;
+        }
+        return -1;
+    }
+
+    void put(int key, int value) {
+
+        //如果key已经存在
+        if (map.count(key) != 0) {
+            auto pos = map[key];
+            list.erase(pos);
+
+            list.push_front(pair<int, int>(key, value));
+            map[key] = list.begin();
+        }
+        else {
+            if (map.size() == capacity) {
+                pair<int, int> last = list.back();
+                
+                //先把最后那个删除
+                list.pop_back();
+                map.erase(last.first);
+
+                list.push_front(pair<int, int>(key, value));
+                map[key] = list.begin();
+            }
+            else {
+                list.push_front(pair<int, int>(key, value));
+                map[key] = list.begin();
+            }
+        }
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
---
+++ LeetCode/stack_queue/155.MinStack/155.MinStack.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+class MinStack {
+private:
+    stack<int> stk;
+    stack<int> minStk;
+
+public:
+    MinStack() {
+
+    }
+
+    void push(int val) {
+
+        stk.push(val);
+
+        if (minStk.size() > 0) {
+            int topMin = minStk.top();
+            if (topMin < val) {
+                minStk.push(topMin);
+            }
+            else {
+                minStk.push(val);
+            }
+        }
+        else {
+            minStk.push(val);
+        }
+    }
+
+    void pop() {
+        stk.pop();
+        minStk.pop();
+    }
+
+    int top() {
+        return stk.top();
+    }
+
+    int getMin() {
+        return minStk.top();
+    }
+};
+
+int main()
+{
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+
+    minStack.getMin();  // -- > 返回 - 3.
+    minStack.pop();
+    minStack.top();     // -- > 返回 0.
+    minStack.getMin();  // -- > 返回 - 2.
+}
