26b3123c1f1cda340c7e898758b7d1f6b9aefa76
WangXi@College <xicser@126.com>
2022-04-08 19:56:22 +0800

    add 23, 124.

--- LeetCode/list/23.MergeKSortedLists/main.cpp
+++ LeetCode/list/23.MergeKSortedLists/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -11,44 +12,86 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+//class Solution {
+//public:
+//    ListNode* mergeKLists(vector<ListNode*>& lists) {
+//
+//        if (lists.size() == 0) {
+//            return nullptr;
+//        }
+//        if (lists.size() == 1) {
+//            return lists[0];
+//        }
+//
+//        ListNode* list = lists[0];
+//        for (unsigned int i = 1; i < lists.size(); i++) {
+//            list = mergeTwoLists(list, lists[i]);
+//        }
+//
+//        return list;
+//    }
+//
+//    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//
+//        if (list1 == nullptr) {
+//            return list2;
+//        }
+//        if (list2 == nullptr) {
+//            return list1;
+//        }
+//
+//        if (list1->val < list2->val) {
+//            list1->next = mergeTwoLists(list1->next, list2);
+//            return list1;
+//        }
+//        else {
+//            list2->next = mergeTwoLists(list1, list2->next);
+//            return list2;
+//        }
+//    }
+//};
+
+
 class Solution {
+private:
+    struct cmp {
+        //小根堆
+        bool operator()(ListNode* l1, ListNode* l2) {
+            return l1->val > l2->val;
+        }
+    };
+
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
 
-        if (lists.size() == 0) {
-            return nullptr;
-        }
-        if (lists.size() == 1) {
-            return lists[0];
+        //小根堆
+        priority_queue<ListNode*, vector<ListNode*>, cmp> que;
+
+        for (auto list : lists) {
+            if (list != nullptr) {
+                que.push(list);
+            }
         }
 
-        ListNode* list = lists[0];
-        for (unsigned int i = 1; i < lists.size(); i++) {
-            list = mergeTwoLists(list, lists[i]);
+        ListNode dummy(-1, nullptr);  //伪头节点
+        ListNode* pCur = &dummy;
+
+        while (que.empty() == false) {
+            ListNode* l = que.top();
+            que.pop();
+
+            pCur->next = l;
+            pCur = l;
+
+            if (l->next != nullptr) {
+                que.push(l->next);
+            }
         }
 
-        return list;
-    }
-
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
-        if (list1 == nullptr) {
-            return list2;
-        }
-        if (list2 == nullptr) {
-            return list1;
-        }
-
-        if (list1->val < list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
-        }
-        else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
-        }
+        return dummy.next;
     }
 };
+
 
 int main()
 {
---
+++ LeetCode/tree/124.BinaryTreeMaximumPathSum/124.BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+private:
+    int maxSum;
+
+public:
+    int maxPathSum(TreeNode* root) {
+        this->maxSum = INT_MIN;
+        process(root);
+        return this->maxSum;
+    }
+
+    //函数含义: 返回一边包含root最大的
+    int process(TreeNode* root) {
+
+        if (root == nullptr) {
+            return 0;
+        }
+
+        int rootVal = root->val;
+        int leftVal = max(process(root->left), 0);    //如果子树返回了负数, 那会越加越小, 那还不如不加, 即取0
+        int rightVal = max(process(root->right), 0);
+
+        //在遍历过程中更新maxSum
+        this->maxSum = max(this->maxSum, rootVal + leftVal + rightVal);
+
+        return max(leftVal + rootVal, rightVal + rootVal);
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
