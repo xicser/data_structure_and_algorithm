6612691b91354fc887908937e4f826982e8e94aa
WangXi@College <xicser@126.com>
2022-04-11 20:54:08 +0800

    add some codes.

---
+++ ACM/多多的字符变换/多多的字符变换.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string& word1, string& word2) {
+
+        sort(word1.begin(), word1.end());
+        sort(word2.begin(), word2.end());
+
+        int len = word1.size();
+
+        //dp[i]表示将word1[0:i]转换成word2[0:i]所需要的最小步骤
+        vector<int> dp(len);
+        dp[0] = abs(word1[0] - word2[0]);
+
+        //dp数组初始化
+        for (int i = 1; i < len; i++) {
+            
+            if (word1[i] == word2[i]) {
+                dp[i] = dp[i - 1];
+            }
+            else {
+                dp[i] = dp[i - 1] + abs(word1[i] - word2[i]);
+            }
+        }
+
+        return dp[len - 1];
+    }
+};
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    Solution sol;
+    cout << sol.minDistance(str1, str2) << endl;
+}
---
+++ LeetCode/list/143.ReorderList/143.ReorderList.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+private:
+    ListNode* split(ListNode* list) {
+
+        ListNode* slow = list, *fast = list->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* next = slow->next;
+        slow->next = nullptr;
+
+        return next;
+    }
+
+    ListNode* reverse(ListNode* list) {
+
+        ListNode* pPre = nullptr;
+        ListNode* pCur = list;
+
+        while (pCur != nullptr) {
+            ListNode* next = pCur->next;
+            pCur->next = pPre;
+            pPre = pCur;
+            pCur = next;
+        }
+
+        return pPre;
+    }
+
+public:
+    void reorderList(ListNode*& head) {
+
+        ListNode* l1 = head;
+        ListNode* l2 = split(l1);
+        l2 = reverse(l2);
+
+        ListNode dummy;
+
+        ListNode* pCur = &dummy;
+
+        bool which = true;
+
+        //合并
+        while (l1 != nullptr && l2 != nullptr) {
+
+            if (which == true) {
+                pCur->next = l1;
+                pCur = l1;
+
+                l1 = l1->next;
+                which = false;
+            }
+            else {
+                pCur->next = l2;
+                pCur = l2;
+
+                l2 = l2->next;
+                which = true;
+            }
+        }
+
+        if (l1 == nullptr) {
+            pCur->next = l2;
+        }
+        else if (l2 == nullptr) {
+            pCur->next = l1;
+        }
+
+        head = dummy.next;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
