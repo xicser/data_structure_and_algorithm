7bbc652212840efe46543bc66f64bc1fae9dcfd4
WangXi@College <xicser@126.com>
2022-04-12 15:19:38 +0800

    add acm.

---
+++ ACM/第K小子串/第K小子串.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    int k;
+    cin >> k;
+
+    set<string> s;
+    for (int i = 0; i < str.size(); i++) {
+        for (int j = i; j < str.size(); j++) {
+            
+            string sub = str.substr(i, j - i + 1);
+
+            if (s.count(sub) != 0) {
+                continue;
+            }
+
+            if (s.size() < k) {
+                s.insert(sub);
+            }
+            else {
+
+                //如果当前sub大于等于set的最后一个元素, 则没有必要insert
+                string endStr = *(--s.end());
+                if (sub >= endStr) {
+                    continue;
+                }
+
+                s.insert(sub);
+                s.erase(--s.end());
+            }
+        }
+    }
+
+    cout << *(--s.end()) << endl;
+
+    return 0;
+}
+
+
+//cout << sub << endl;
