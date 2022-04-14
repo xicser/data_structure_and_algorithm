bacb000ca21bc69c3091abc9ec728d0a9f92b4ea
WangXi@College <xicser@126.com>
2022-04-14 21:41:43 +0800

    add codes.

---
+++ ACM/万万没想到之聪明的编辑/万万没想到之聪明的编辑.cpp
@@ -0,0 +1,79 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <string>
+#include <string.h>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <vector>
+#include <stack>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+// wooooooow
+string copeStr(string& str) {
+
+	string result;
+	int index = 0;
+	for (int i = 0; i < str.size(); i++) {
+		if (result.size() >= 2) {
+			if (result.size() == 2) {
+				char a = result[index - 2];
+				char b = result[index - 1];
+				char c = str[i];
+				if (a == b && b == c) {
+					continue;
+				}
+
+				result.push_back(c);
+				index++;
+			}
+			else {
+				char a = result[index - 3];
+				char b = result[index - 2];
+				char c = result[index - 1];
+				char d = str[i];
+
+				if (b == c && c == d) {
+					continue;
+				}
+
+				if (a == b && b != c && c == d) {
+					continue;
+				}
+				
+				result.push_back(d);
+				index++;
+			}
+		}
+		else {
+			result.push_back(str[i]);
+			index++;
+		}
+	}
+
+	return result;
+}
+
+int main() {
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		string s;
+		cin >> s;
+		cout << copeStr(s) << endl;
+	}
+
+	return 0;
+}
+
