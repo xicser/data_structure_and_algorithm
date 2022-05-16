2bdd8d621781f39f1710d73d8327acb0002d0520
WangXi@Laptop <xicser@126.com>
2022-05-16 23:37:58 +0800

    add some problems.

---
+++ ACM/寻找子串.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+unordered_set<string> st;
+
+//计算next数组
+void makeNext(const string& pattern, vector<int>& next) {
+    int len = 0;  //len表示index之前的最长相等前后缀的长度
+    next[0] = 0;
+    for (int index = 1; index < pattern.size(); index++) {
+
+        while (len > 0 && pattern[len] != pattern[index]) {
+            len = next[len - 1];
+        }
+        if (pattern[len] == pattern[index]) {
+            len++;
+        }
+
+        next[index] = len;
+    }
+}
+
+struct Interval
+{
+    int left, right;
+    Interval(int _left, int _right) {
+        left = _left;
+        right = _right;
+    }
+};
+vector<Interval> result;
+void kmp(const string& text, const string& pattern)
+{
+    if (pattern.size() > text.size()) {
+        return;
+    }
+
+    vector<int> next(pattern.size(), 0);
+    makeNext(pattern, next);
+
+    int index = -1;
+
+    int i = 0, j = 0;  //i指向text, j指向pattern
+    while (i < text.size()) {
+
+        if (j == pattern.size() - 1 && pattern[j] == text[i]) {
+            index = i - j;
+            
+            result.push_back(Interval(index, index + pattern.size() - 1));
+
+            j = 0;
+            i++;
+
+            continue;
+        }
+
+        if (text[i] == pattern[j]) {
+            i++;
+            j++;
+        }
+        else {
+            //得先找到第一个位置
+            if (j == 0) {
+                i++;
+            }
+            else {
+                j = next[j - 1];
+            }
+        }
+    }
+}
+
+int eraseOverlapIntervals(vector<Interval>& intervals) {
+    sort(intervals.begin(), intervals.end(), [=](const Interval& I1, const Interval& I2) {
+        //先按照左端点从大到小
+        if (I1.left > I2.left) {
+            return true;
+        }
+        //若左端点相同, 则按照右端点从小到大
+        else if (I1.left == I2.left && I1.right < I2.right) {
+            return true;
+        }
+        return false;
+    });
+
+    int result = 1; //至少需要一支箭
+    int lastLeft = intervals[0].left;
+    for (unsigned int i = 1; i < intervals.size(); i++) {
+        int right = intervals[i].right;
+        if (right < lastLeft) {
+            result++;
+            lastLeft = intervals[i].left;
+        }
+    }
+
+    return result;
+}
+int main()
+{
+    int m;
+    cin >> m;
+
+    for (int i = 0; i < m; i++) {
+        string tempStr;
+        cin >> tempStr;
+        st.insert(tempStr);
+    }
+
+    string str;
+    cin >> str;
+
+    //先看看每个子串在str的区间范围
+    for (auto& stri : st) {
+        kmp(str, stri);
+    }
+
+    //选区间问题
+    cout << eraseOverlapIntervals(result) << endl;
+
+    return 0;
+}
---
+++ ACM/紧急疏散.cpp
@@ -0,0 +1,154 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+
+using namespace std;
+
+int vertexCount;
+
+//邻接表
+unordered_map<int, vector<int> > adjList;
+
+int dfs(int startVertexId, vector<bool>& isVisited);
+int main()
+{
+    int n;
+    cin >> n;
+
+    vertexCount = n;
+
+    //顶点访问标记
+    vector<bool> isVisited(vertexCount, false);
+
+    for (int i = 0; i < vertexCount - 1; i++) {
+
+        int node1, node2;
+        cin >> node1 >> node2;
+        node1--;
+        node2--;
+
+        int key = node1;
+        vector<int> &vec1 = adjList[key];
+        vec1.push_back(node2);
+        //adjList[key] = vec1;
+
+        key = node2;
+        vector<int> &vec2 = adjList[key];
+        vec2.push_back(node1);
+        //adjList[key] = vec2;
+    }
+
+    //遍历根节点的子节点, 看哪个节点个数最多
+    isVisited[0] = true;    //根节点已经访问过
+    int maxVal = 0;
+
+    vector<int> rootNeibor = adjList[0];
+    for (int next : rootNeibor) {
+        
+        int temp = dfs(next, isVisited);
+        maxVal = max(temp, maxVal);
+    }
+
+    cout << maxVal << endl;
+
+    return 0;
+}
+
+//返回当前这个树的节点个数
+int dfs(int startVertexId, vector<bool>& isVisited)
+{
+    if (isVisited[startVertexId] == true) {
+        return 0;
+    }
+
+    isVisited[startVertexId] = true;             //标记该顶点已经被访问过
+
+    int maxVal = 0;
+    int key = startVertexId;
+    vector<int> rootNeibor = adjList[key];
+    for (int next : rootNeibor) {
+        int temp = dfs(next, isVisited);
+        maxVal += temp;
+    }
+
+    return maxVal + 1;
+}
+
+/*
+* 
+* 邻接矩阵做法：
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+
+using namespace std;
+
+int vertexCount;
+
+int dfs(int startVertexId, vector<bool>& isVisited, vector< vector<bool> >& matrix);
+int main()
+{
+    int n;
+    cin >> n;
+
+    vertexCount = n;
+
+    vector< vector<bool> > matrix(vertexCount, vector<bool>(vertexCount, false));
+
+    //顶点访问标记
+    vector<bool> isVisited(vertexCount, false);
+
+    for (int i = 0; i < vertexCount - 1; i++) {
+
+        int node1, node2;
+        cin >> node1 >> node2;
+        node1--;
+        node2--;
+
+        matrix[node1][node2] = true;
+        matrix[node2][node1] = true;
+    }
+
+    //遍历根节点的子节点, 看哪个节点个数最多
+    isVisited[0] = true;    //根节点已经访问过
+    int maxVal = 0;
+    for (int i = 0; i < vertexCount; i++) {
+
+        //如果和当前顶点有通路, 并且没有被访问过, 则访问它
+        if (matrix[0][i] == true) {
+            int temp = dfs(i, isVisited, matrix);
+            maxVal = max(temp, maxVal);
+        }
+    }
+
+    cout << maxVal << endl;
+
+    return 0;
+}
+
+//返回当前这个树的节点个数
+int dfs(int startVertexId, vector<bool>& isVisited, vector< vector<bool> >& matrix)
+{
+    if (isVisited[startVertexId] == true) {
+        return 0;
+    }
+
+    isVisited[startVertexId] = true;             //标记该顶点已经被访问过
+
+    int maxVal = 0;
+    for (int i = 0; i < vertexCount; i++) {
+
+        //如果和当前顶点有通路, 并且没有被访问过, 则访问它
+        if (matrix[startVertexId][i] == true) {
+            int temp = dfs(i, isVisited, matrix);
+            maxVal += temp;
+        }
+    }
+
+    return maxVal + 1;
+}
+
+*/
