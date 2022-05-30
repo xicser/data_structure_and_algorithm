cc0c67ed866f5e6dffe16b40e2137c31de7f6be4
WangXi@Laptop <xicser@126.com>
2022-05-30 11:48:21 +0800

    add some codes.

---
+++ LeetCode/basic/470.ImplementRand10()UsingRand7()/470.ImplementRand10()UsingRand7().cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+int rand7()
+{
+
+}
+class Solution {
+public:
+    int rand10() {
+        int i = rand7() - 1;
+        int j = rand7() - 1;
+
+        //val的范围是0到49
+        int val = i * 7 + j;
+        
+        if (val >= 40) {
+            return rand10();
+        }
+        else {
+            return val % 10 + 1;
+        }
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
