54ed6835d5f482687f334dba2b73438dc66655f4
WangXi@College <xicser@126.com>
2022-06-08 12:01:55 +0800

    add some codes.

---
+++ LeetCode/binary_search/852.PeakIndexInAMountainArray/852.PeakIndexInAMountainArray.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+
+        int left = 0, right = arr.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            if (arr[mid - 1] < arr[mid]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+};
+
+int main()
+{
+    vector<int> arr = { 24,69,100,99,79,78,67,36,26,19 };
+    Solution sol;
+
+    std::cout << sol.peakIndexInMountainArray(arr) << endl;
+}
---
+++ LeetCode/list/138.CopyListWithRandomPointer/138.CopyListWithRandomPointer.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+        if (head == nullptr) {
+            return nullptr;
+        }
+
+        unordered_map<Node*, Node*> nodeMap;
+
+        Node dummy(-1);
+
+        Node* pCur = head;
+        Node* pCurNew = &dummy;
+
+        // 先复制一份
+        while (pCur != nullptr) {
+
+            Node* newNode = new Node(pCur->val);
+            newNode->next = nullptr;
+
+            pCurNew->next = newNode;
+
+            pCurNew = newNode;
+
+            nodeMap[pCur] = newNode;
+
+            pCur = pCur->next;
+        }
+
+        //再处理random指针
+        pCur = head;
+        while (pCur != nullptr) {
+            Node* copy = nodeMap[pCur];
+            copy->random = nodeMap[pCur->random];
+
+            pCur = pCur->next;
+        }
+
+        return dummy.next;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
